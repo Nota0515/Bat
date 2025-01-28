076e64dd5185e7c4faa75cfc56ccdab7f946a206
Nota0515 <businessbro78@gmail.com>
2025-01-28 23:10:46 +0530

    added images and developing project section

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -2,7 +2,13 @@
 const Projects = () => {
   return (
     <div className="projects-section flex justify-center items-center min-h-[100dvh] bg-n-3 ">
-      <div className="flex min-h-[59.563rem] items-center justify-center"></div>
+      <div className="flex min-h-[59.563rem] items-center justify-center">
+        <div className="section2-container flex flex-col lg:flex-row px-5 py-5 mt-2">
+          <div className="headphones-image max-w-[37rem] max-h-[37rem]">
+            <img src="src\assets\headphones\headphones.png" alt="headphonesimg" className="w-full h-full object-cover object-center" />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
